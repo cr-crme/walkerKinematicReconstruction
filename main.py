dbc3fd01605f7502aad63e133fb23a72bd7ae6cb
Pariterre <pariterre@hotmail.com>
2022-08-02 12:58:12 -0400

    Creation of the model from the c3d using biorbd

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-import os
-
-import biorbd
-from biorbd import Segment, Marker, KinematicModelGeneric,Axis
+from biorbd import KinematicModelGeneric, Axis
 import bioviz
 import ezc3d
 
+
 kinematic_model_file_path = "temporary.bioMod"
 c3d = ezc3d.c3d("data/pilote/Audrey_19_mai_statique.c3d")
-
-
-# 'LFHD', 'RFHD', 'LBHD', 'RBHD', 'C7', 'T10', 'CLAV', 'STRN', 'RBAK', 'LSHO', 'LELB', 'LWRA', 'LWRB', 'LFIN', 'RSHO',
-# 'RELB', 'RWRA', 'RWRB', 'RFIN', 'LASI', 'RASI', 'LPSI', 'RPSI', 'LTHI', 'LKNE', 'LTIB', 'LANK', 'LHEE', 'LTOE',
-# 'RTHI', 'RKNE', 'RTIB', 'RANK', 'RHEE', 'RTOE', 'LHUM', 'RHUM', 'LTROC', 'RTROC', 'LTHID', 'RTHID', 'LKNM', 'RKNM',
-# 'LTIBP', 'LTIBD', 'RTIBP', 'RTIBD', 'LANKM', 'L5MH', 'RANKM', 'R5MH', '*51'
 
 model = KinematicModelGeneric()
 model.add_segment("PELVIS", translations="yz", rotations="x")
@@ -25,86 +17,208 @@ model.set_rt(
     second_axis_markers=(("LPSI", "RPSI", "LASI", "RASI"), ("RPSI", "RASI")),
     axis_to_keep=Axis.Name.Y,
 )
+model.add_marker("PELVIS", "mid_pelvis", ("LPSI", "RPSI", "LASI", "RASI"))
+model.add_marker("PELVIS", "LPSI")
+model.add_marker("PELVIS", "RPSI")
+model.add_marker("PELVIS", "LASI")
+model.add_marker("PELVIS", "RASI")
+
+model.add_segment("TRUNK", parent_name="PELVIS", rotations="x")
+model.set_rt(
+    segment_name="TRUNK",
+    origin_markers="CLAV",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=(("T10", "STRN"), ("C7", "CLAV")),
+    second_axis_name=Axis.Name.Y,
+    second_axis_markers=(("T10", "C7"), ("STRN", "CLAV")),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("TRUNK", "T10")
+model.add_marker("TRUNK", "C7")
+model.add_marker("TRUNK", "STRN")
+model.add_marker("TRUNK", "CLAV")
+
+model.add_segment("HEAD", parent_name="TRUNK")
+model.set_rt(
+    segment_name="HEAD",
+    origin_markers=("LBHD", "RBHD", "LFHD", "RFHD"),
+    first_axis_name=Axis.Name.X,
+    first_axis_markers=(("LBHD", "LFHD"), ("RBHD", "RFHD")),
+    second_axis_name=Axis.Name.Y,
+    second_axis_markers=(("LBHD", "RBHD"), ("LFHD", "RFHD")),
+    axis_to_keep=Axis.Name.Y,
+)
+model.add_marker("HEAD", "LBHD")
+model.add_marker("HEAD", "RBHD")
+model.add_marker("HEAD", "LFHD")
+model.add_marker("HEAD", "RFHD")
+
+model.add_segment("RUPPER_ARM", parent_name="TRUNK", rotations="zx")
+model.set_rt(
+    segment_name="RUPPER_ARM",
+    origin_markers="RSHO",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=("RELB", "RSHO"),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("RWRB", "RWRA"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("RUPPER_ARM", "RSHO")
+model.add_marker("RUPPER_ARM", "RELB")
+
+model.add_segment("RLOWER_ARM", parent_name="RUPPER_ARM", rotations="x")
+model.set_rt(
+    segment_name="RLOWER_ARM",
+    origin_markers="RELB",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=(("RWRB", "RWRA"), "RELB"),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("RWRB", "RWRA"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("RLOWER_ARM", "RWRB")
+model.add_marker("RLOWER_ARM", "RWRA")
+
+model.add_segment("RHAND", parent_name="RLOWER_ARM", rotations="x")
+model.set_rt(
+    segment_name="RHAND",
+    origin_markers=("RWRB", "RWRA"),
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=("RFIN", ("RWRB", "RWRA")),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("RWRB", "RWRA"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("RHAND", "RFIN")
+
+model.add_segment("LUPPER_ARM", parent_name="TRUNK", rotations="zx")
+model.set_rt(
+    segment_name="LUPPER_ARM",
+    origin_markers="LSHO",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=("LELB", "LSHO"),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("LWRB", "LWRA"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("LUPPER_ARM", "LSHO")
+model.add_marker("LUPPER_ARM", "LELB")
+
+model.add_segment("LLOWER_ARM", parent_name="LUPPER_ARM", rotations="x")
+model.set_rt(
+    segment_name="LLOWER_ARM",
+    origin_markers="LELB",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=(("LWRB", "LWRA"), "LELB"),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("LWRB", "LWRA"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("LLOWER_ARM", "LWRB")
+model.add_marker("LLOWER_ARM", "LWRA")
+
+model.add_segment("LHAND", parent_name="LLOWER_ARM", rotations="x")
+model.set_rt(
+    segment_name="LHAND",
+    origin_markers=("LWRB", "LWRA"),
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=("LFIN", ("LWRB", "LWRA")),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("LWRB", "LWRA"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("LHAND", "LFIN")
+
+model.add_segment("RTHIGH", parent_name="PELVIS", rotations="x")
+model.set_rt(
+    segment_name="RTHIGH",
+    origin_markers="RTROC",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=("RKNE", "RTROC"),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("RKNM", "RKNE"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("RTHIGH", "RTROC")
+model.add_marker("RTHIGH", "RKNE")
+model.add_marker("RTHIGH", "RKNM")
+
+model.add_segment("RLEG", parent_name="RTHIGH", rotations="x")
+model.set_rt(
+    segment_name="RLEG",
+    origin_markers=("RKNM", "RKNE"),
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=(("RANKM", "RANK"), ("RKNM", "RKNE")),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("RKNM", "RKNE"),
+    axis_to_keep=Axis.Name.X,
+)
+model.add_marker("RLEG", "RANKM")
+model.add_marker("RLEG", "RANK")
+
+model.add_segment("RFOOT", parent_name="RLEG", rotations="x")
+model.set_rt(
+    segment_name="RFOOT",
+    origin_markers=("RANKM", "RANK"),
+    first_axis_name=Axis.Name.X,
+    first_axis_markers=("RANKM", "RANK"),
+    second_axis_name=Axis.Name.Y,
+    second_axis_markers=(("RANKM", "RANK"), "RTOE"),
+    axis_to_keep=Axis.Name.X,
+)
+model.add_marker("RFOOT", "RTOE")
+model.add_marker("RFOOT", "R5MH")
+
+model.add_segment("LTHIGH", parent_name="PELVIS", rotations="x")
+model.set_rt(
+    segment_name="LTHIGH",
+    origin_markers="LTROC",
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=("LKNE", "LTROC"),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("LKNE", "LKNM"),
+    axis_to_keep=Axis.Name.Z,
+)
+model.add_marker("LTHIGH", "LTROC")
+model.add_marker("LTHIGH", "LKNE")
+model.add_marker("LTHIGH", "LKNM")
+
+model.add_segment("LLEG", parent_name="LTHIGH", rotations="x")
+model.set_rt(
+    segment_name="LLEG",
+    origin_markers=("LKNM", "LKNE"),
+    first_axis_name=Axis.Name.Z,
+    first_axis_markers=(("LANKM", "LANK"), ("LKNM", "LKNE")),
+    second_axis_name=Axis.Name.X,
+    second_axis_markers=("LKNE", "LKNM"),
+    axis_to_keep=Axis.Name.X,
+)
+model.add_marker("LLEG", "LANKM")
+model.add_marker("LLEG", "LANK")
+
+model.add_segment("LFOOT", parent_name="LLEG", rotations="x")
+model.set_rt(
+    segment_name="LFOOT",
+    origin_markers=("LANKM", "LANK"),
+    first_axis_name=Axis.Name.X,
+    first_axis_markers=("LANK", "LANKM"),
+    second_axis_name=Axis.Name.Y,
+    second_axis_markers=(("LANKM", "LANK"), "LTOE"),
+    axis_to_keep=Axis.Name.X,
+)
+model.add_marker("LFOOT", "LTOE")
+model.add_marker("LFOOT", "L5MH")
+
+# 'LFHD', 'RFHD', 'LBHD', 'RBHD', 'C7', 'T10', 'CLAV', 'STRN', 'RBAK', 'LSHO', 'LELB', 'LWRA', 'LWRB', 'LFIN', 'RSHO',
+# 'RELB', 'RWRA', 'RWRB', 'RFIN', 'LASI', 'RASI', 'LPSI', 'RPSI', 'LTHI', 'LKNE', 'LTIB', 'LANK', 'LHEE', 'LTOE',
+# 'RTHI', 'RKNE', 'RTIB', 'RANK', 'RHEE', 'RTOE', 'LHUM', 'RHUM', 'LTROC', 'RTROC', 'LTHID', 'RTHID', 'LKNM', 'RKNM',
+# 'LTIBP', 'LTIBD', 'RTIBP', 'RTIBD', 'LANKM', 'L5MH', 'RANKM', 'R5MH', '*51'
 
 # Collapse the model to an actual personalized model
-model.generate_personalized(c3d, "temporary.bioMod")
+model.generate_personalized(c3d, kinematic_model_file_path)
 
-#
-# # The trunk segment
-# mid_back_trunk = Marker.from_data(
-#     c3d=c3d,
-#     name="PELVIS",
-#     data_names=("LFHD",),
-#     parent_name="TRUNK",
-# )
-# trunk = Segment(
-#     name="TRUNK",
-#     translations="yz",
-#     rotations="x",
-#     mesh=((0, 0, 0), (0, 0, 0.53)),
-#     markers=(trunk_marker_pelvis,),
-# )
-#
-# # The head segment
-# top_head_marker_head = Marker.from_data(
-#     data=data,
-#     name="TOP_HEAD",
-#     parent_name="HEAD",
-# )
-# head = Segment(
-#     name="HEAD",
-#     parent_name="TRUNK",
-#     rt="0 0 0 xyz 0 0 0.53",
-#     mesh=((0, 0, 0), (0, 0, 0.24)),
-#     markers=(top_head_marker_head,),
-# )
-#
-# # The arm segment
-# shoulder_marker = Marker(
-#     name="SHOULDER",
-#     parent_name="UPPER_ARM",
-#     position=(0, 0, 0),
-# )
-# upper_arm = Segment(
-#     name="UPPER_ARM",
-#     parent_name=trunk.name,
-#     rt="0 0 0 xyz 0 0 0.53",
-#     rotations="x",
-#     mesh=((0, 0, 0), (0, 0, -0.28)),
-#     markers=(shoulder_marker,),
-# )
-#
-# elbow_marker = Marker(
-#     name="ELBOW",
-#     parent_name="LOWER_ARM",
-#     position=(0, 0, 0),
-# )
-# lower_arm = Segment(
-#     name="LOWER_ARM",
-#     parent_name=upper_arm.name,
-#     rt="0 0 0 xyz 0 0 -0.28",
-#     mesh=((0, 0, 0), (0, 0, -0.27)),
-#     markers=(elbow_marker,),
-# )
-#
-# wrist_marker = Marker(
-#     name="WRIST",
-#     parent_name="HAND",
-#     position=(0, 0, 0),
-# )
-# finger_marker = Marker(
-#     name="FINGER",
-#     parent_name="HAND",
-#     position=(0, 0, -0.19),
-# )
-# hand = Segment(
-#     name="HAND",
-#     parent_name=lower_arm.name,
-#     rt="0 0 0 xyz 0 0 -0.27",
-#     mesh=((0, 0, 0), (0, 0, -0.19)),
-#     markers=(wrist_marker, finger_marker)
-# )
-#
+bioviz.Viz(kinematic_model_file_path).exec()
+
 # # The thigh segment
 # thigh = Segment(
 #     name="THIGH",
