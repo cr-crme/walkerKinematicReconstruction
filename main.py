f6443cc479a7a51d16dfdd64cc6ab3fc615462fd
Pariterre <pariterre@hotmail.com>
2023-02-10 18:38:04 -0500

    First visual analisys

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from walker import BiomechanicsTools
 
 # Options
 kinematic_model_file_path = "temporary.bioMod"
-static_trial = "data/pilote/Audrey_19_mai_statique.c3d"
-trials = ("data/pilote/Audrey_19_mai_marche4.c3d", "data/pilote/Audrey_19_mai_marche6.c3d")
+static_trial = "data/pilote2/2023-01-19_AP_test_statique_TROCH.c3d"
+trials = ("data/pilote2/2023-01-19_AP_test_marchecrouch_05.c3d", "data/pilote2/2023-01-19_AP_test_marchecrouch_06.c3d",)
 
 # Generate the personalized kinematic model
 tools = BiomechanicsTools(body_mass=100)
--- main_missing_markers_comparison.py
+++ main_missing_markers_comparison.py
@@ -1,10 +1,13 @@
+import os
+
 from walker import BiomechanicsTools
 from matplotlib import pyplot as plt
+import numpy as np
 
 from c3d_modifier import remove_markers
 
 
-# Options
+# --- Options --- #
 kinematic_model_file_path = "temporary.bioMod"
 static_trial = "data/pilote2/2023-01-19_AP_test_statique_TROCH.c3d"
 trials = (
@@ -12,28 +15,38 @@ trials = (
 )
 markers_to_remove = (
     (),
-    ('LPSI', 'RPSI', 'LASI', 'RASI'),
+    ("LPSI", "RPSI", "LASI", "RASI"),
 )
+colors = ("r", "b")
+dof_to_compare = ("RFemur_RotY", "RTibia_RotY", "RFoot_RotY", "LFemur_RotY", "LTibia_RotY", "LFoot_RotY",)
+# --------------- #
+
+# Sanity check
+if len(colors) != len(markers_to_remove):
+    raise ValueError("The number of conditions should match")
 
 # Generate the personalized kinematic model
 tools = BiomechanicsTools(body_mass=100)
 tools.personalize_model(static_trial, kinematic_model_file_path)
 
 # Perform some biomechanical computation
-q = []
+all_q = []
 for trial in trials:
     q_tp = []
     for markers in markers_to_remove:
         remove_markers(trial, "tp.c3d", markers)
         tools.process_trial("tp.c3d", compute_automatic_events=False, only_compute_kinematics=True)
         q_tp.append(tools.q)
-    q.append(q_tp)
+        os.remove("tp.c3d")
+    all_q.append(q_tp)
 
-plt.plot(q[0][0][0, :], 'r')
-plt.plot(q[0][1][0, :], 'b')
+# TODO : reconstruct pelvis relative to vertical
+
+for name in dof_to_compare:
+    dof = tuple(n.to_string() for n in tools.model.nameDof()).index(name)
+    plt.figure()
+    plt.title(f"DoF: {name}")
+    for q in all_q:
+        for condition, color in zip(q, colors):
+            plt.plot(np.unwrap(condition[dof, :]) * 180 / np.pi, color)
 plt.show()
-
-
-
-
-
